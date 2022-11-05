d256e1ee66886184330222657abc11fa74ec7da8
Darthwimp <debargha2003.das@gmail.com>
2022-11-05 16:29:02 +0530

    added custom widget

---
+++ lib/widgets/note_card.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_app/styles/styles.dart';
+
+
+Widget note_card(Function()? onTap, QueryDocumentSnapshot doc) => InkWell(
+    onTap: () {},
+    child: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        height: 200,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: cardsColor[doc["color_id"]],
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          children: [
+            Text(doc["note_title"], style: noteTitle,),
+            Text(doc["creation_date"], style: creationDate,),
+            Text(doc["note_content"], style: noteContent, overflow: TextOverflow.fade),
+          ],
+        ),
+      ),
+    ),
+  );
