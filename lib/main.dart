52474617d55553fcff2b2413806926adbc089dbe
Darthwimp <debargha2003.das@gmail.com>
2022-11-05 19:31:08 +0530

    bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:notes_app/screens/homePage.dart';
-import 'package:notes_app/theme/themes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,20 +9,12 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: iconSwitch ? lightTheme : darkTheme,
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    );
+    return const MaterialApp(
+        home: HomePage(), debugShowCheckedModeBanner: false);
   }
 }
--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -7,7 +7,6 @@ import 'package:notes_app/styles/styles.dart';
 import 'package:notes_app/theme/themes.dart';
 import 'package:notes_app/widgets/note_card.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -18,78 +17,86 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.library_books_rounded),
-        title: const Text("Notes"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  iconSwitch = !iconSwitch;
-                });
-              },
-              icon: Icon(iconSwitch ? iconLight : iconDark))
-        ],
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox( height: 10,),
-            Text(
-              "Your Recent Texts",
-              style: defaultFont,
-            ),
-            const SizedBox(height: 20),
-            StreamBuilder<QuerySnapshot>(
-              stream:
-                  FirebaseFirestore.instance.collection("Notes").snapshots(),
-              builder: ((context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (snapshot.hasData) {
-                  return Column(
-                    children: snapshot.data!.docs
-                        .map(
-                          (note) => note_card(() {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => InputPage(note),
-                                ));
-                          }, note),
-                        )
-                        .toList(),
-                  );
-                }
-                return Text(
-                  "There's no Text :(",
-                  style: defaultFont,
-                );
-              }),
-            )
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: iconSwitch ? lightTheme : darkTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.library_books_rounded),
+          title: const Text("Notes"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    iconSwitch = !iconSwitch;
+                  });
+                },
+                icon: Icon(iconSwitch ? iconLight : iconDark))
           ],
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => InputEditor()));
-        },
-        icon: const Icon(
-          Icons.add,
-          size: 33,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Your Recent Texts",
+                style: defaultFont,
+              ),
+              const SizedBox(height: 20),
+              StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection("Notes").snapshots(),
+                builder: ((context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: snapshot.data!.docs
+                            .map(
+                              (note) => note_card(() {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => InputPage(note),
+                                    ));
+                              }, note),
+                            )
+                            .toList(),
+                      ),
+                    );
+                  }
+                  return Text(
+                    "There's no Text :(",
+                    style: defaultFont,
+                  );
+                }),
+              )
+            ],
+          ),
         ),
-        label: Text(
-          "Add more?",
-          style: defaultFont,
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => InputEditor()));
+          },
+          icon: const Icon(
+            Icons.add,
+            size: 33,
+          ),
+          label: Text(
+            "Add more?",
+            style: defaultFont,
+          ),
         ),
       ),
     );
