9f87b5cea6088f919f55350d879f5165a3795a5d
Darthwimp <debargha2003.das@gmail.com>
2022-11-05 15:57:11 +0530

    added styles

---
+++ lib/styles/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+List<Color> cardsColor = [
+  Colors.deepPurple.shade300,
+  Colors.indigo.shade300,
+  Colors.teal.shade300,
+  Colors.lightBlue.shade300,
+  Colors.yellow.shade300,
+  Colors.red.shade300,
+  Colors.brown.shade300,
+];
+
+TextStyle noteTitle =
+    GoogleFonts.poorStory(fontSize: 12, fontWeight: FontWeight.bold);
+TextStyle noteContent =
+    GoogleFonts.poorStory(fontSize: 10, fontWeight: FontWeight.normal);
+TextStyle creationDate =
+    GoogleFonts.roboto(fontSize: 10, fontWeight: FontWeight.bold);
