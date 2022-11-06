1177a50156f0dad0dc7736d500c2f5c494225754
Darthwimp <debargha2003.das@gmail.com>
2022-11-06 10:00:54 +0530

    added delete button

--- lib/screens/inputPage.dart
+++ lib/screens/inputPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:notes_app/styles/styles.dart';
+import 'homePage.dart';
 
 class InputPage extends StatefulWidget {
   InputPage(this.doc, {super.key});
@@ -19,6 +20,17 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         backgroundColor: cardsColor[color_id],
         elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {
+                DocumentReference documentReference =
+                    FirebaseFirestore.instance.collection("Notes").doc();
+                documentReference.delete();
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              icon: const Icon(Icons.delete_forever))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
